b6cbaa5d4fc2c24c0e476eb088b5bd2b95bf6a54
Ryan Williams <arwilliams0022@gmail.com>
2021-05-09 17:13:47 -0400

    Implement schedule command

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ import os
 import discord
 from discord.ext import commands
 import aiohttp
+from datetime import date
+from dateutil.parser import isoparse
+import calendar
 
 bot = commands.Bot(command_prefix='~')
 
 api_base = "https://api.jikan.moe/v3/"
+
+def get_current_day():
+    weekday = date.today().weekday()
+    day_str = calendar.day_name[weekday]
+    return day_str.lower()
+
+def timestamp_to_formatted_time(timestamp):
+    date = isoparse(timestamp)
+    return date.strftime('%-I:%M %p')
 
 @bot.command()
 async def search(ctx, *, query: str):
@@ -44,5 +56,23 @@ async def top(ctx):
                 embed.set_thumbnail(url=result['image_url'])
 
                 await ctx.send(embed=embed)
+
+@bot.command()
+async def schedule(ctx):
+    async with aiohttp.ClientSession() as session:
+        day = get_current_day()
+
+        url = api_base + "schedule/" + day
+        async with session.get(url) as resp:
+            content = await resp.json()
+
+            message = 'Here\'s today\'s schedule:\n'
+
+            for result in content[day]:
+                time = timestamp_to_formatted_time(result['airing_start'])
+                message += f"\n{result['title']} ({time})"
+
+            await ctx.send(message)
+
 
 bot.run(os.getenv('DISCORD_TOKEN'))
